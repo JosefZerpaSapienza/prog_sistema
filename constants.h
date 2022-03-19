695f211ac8fdfced18df37f7ff7bf58d67a15631
Josef <zerparuiz.1837394@studenti.uniroma1.it>
2022-03-19 20:21:46 +0100

    with previous commit

--- constants.h
+++ constants.h
@@ -4,3 +4,18 @@
 #define PASSPHRASE_BUFFER_SIZE 256
 #define MSG_BUFFER_SIZE 1024
 #define SMALL_BUFFER_SIZE 32
+
+// Return value for 'OK/SUCCESS'.
+#define OK 0
+// Return value for 'Invalid parameters'.
+#define PARAM_ERR -1
+// Return value for 'Authentication failed'.
+#define AUTH_FAIL -2
+// Return value for 'Connection error'.
+#define CONN_ERR -3
+// Return value for 'Protocol error'.
+#define PROTO_ERR -4
+// Return value for 'Command not supported'.
+#define COMM_ERR -5
+// Return value for 'Generic internal error'.
+#define INT_ERR -6
